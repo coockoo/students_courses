a6fadbb4291a945b2dad307328fd143137ec842a
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-02-10 23:21:00 +0200

    Final version of student_controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,35 @@
 class StudentsController < ApplicationController
   
+  # /students GET
   def index
     @students = Student.all
-    render text: @students.map { |i| "#{i.name} -- #{i.email}" }.join("<br/>")
   end
 
+  # /students POST
   def create
     @student = Student.create(params[:student])
     render text: "#{@student.id}: #{@student.name}: #{@student.email}"
+  end
+
+  # /students/1 GET
+  def show
+    @student = Student.find_by_id(params[:id])
+  end
+
+  # /students/new GET
+  def new
+  end
+
+  # /students/1/edit GET
+  def edit
+  end
+
+  # /students/1 PUT
+  def update
+  end
+
+  # /students/1 DELETE
+  def destroy
   end
 
 end
