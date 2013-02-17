dfc0e77bbdcf7388f7e1e54b685be7193b50bf70
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-02-17 17:15:49 +0200

    Added validation to models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ActiveRecord::Base
   attr_accessible :title, :descr
   has_and_belongs_to_many :students
+
   validates :title, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ActiveRecord::Base
   attr_accessible :name, :email
   has_and_belongs_to_many :courses
+
   validates :name, :email, presence: true
+  validates_format_of :email, :with => /^\w|[1-9]@[\w]\.[\w]$/
 end
