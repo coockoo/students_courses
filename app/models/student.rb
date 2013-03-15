ad448d8caf32617fc1d25105ae2eaebf0c1f6c0e
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-03-15 02:41:34 +0200

    Changed email validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < ActiveRecord::Base
   has_and_belongs_to_many :courses
 
   validates :name, :email, presence: true
-  validates_format_of :email, :with => /^\w|[1-9]@[\w]\.[\w]$/
+  validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 
   before_create do
    self.id = UUIDTools::UUID.random_create.to_s 
