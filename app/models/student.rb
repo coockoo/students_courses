1f7f9af0dd8e644b9995c23e8519831a6d2844b9
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-03-28 13:56:16 +0200

    Added devise to the model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,12 @@
 class Student < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   set_primary_key 'uuid'
   attr_accessible :name, :email
   has_and_belongs_to_many :courses
