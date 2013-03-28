a7e2fad549be8e5b24d36d88e94f86cb89bc501a
Olexandr Bondarchuk <alex.coockoo@gmail.com>
2013-03-28 13:52:52 +0200

    Change students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
+  before_filter :authenticate_student!, :except => [:index, :new, :show, :create]
+
   # /students GET
   def index
     @students = Student.all
+    @student = Student.new
+    respond_to do |format|
+      format.html
+    end
   end
 
   # /students POST
@@ -10,9 +16,10 @@ class StudentsController < ApplicationController
     @student = Student.new(params[:student])
     respond_to do |format|
       if @student.save
-        format.html { render text: "#{@student.id}: #{@student.name}: #{@student.email} created" } 
+        format.js { render :partial => "new_student", :status => :ok, content_type: 'text/html' }
+        format.html { redirect_to student_path(@student) }
       else
-        format.html { render text: "Something wrong! #{@student.errors.first}" }
+        format.html { render "new" }
       end
     end
   end
@@ -21,13 +28,18 @@ class StudentsController < ApplicationController
   def show
     unless @student = Student.find_by_uuid(params[:id])
       render text: "Page not found", status: 404
+      # render text: params.inspect 
     end
     @courses = @student.courses
   end
 
   # /students/new GET
   def new
-    @student = Student.new(params[:student])
+    @student = Student.new
+    respond_to do |format|
+      format.js { render :partial => "student_form", :status => :ok, content_type: 'text/html' }
+      format.html
+    end
   end
 
   # /students/1/edit GET
@@ -59,6 +71,7 @@ class StudentsController < ApplicationController
 
   # /students/1 DELETE
   def destroy
+    @student.find_by_uuid(params[:id])
   end
 
 end
